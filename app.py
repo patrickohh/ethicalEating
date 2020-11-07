9870604264f8d7d2309dca58c639b003dee492da
Alex Ruma <alexanderruma@gmail.com>
2020-11-07 14:18:47 -0800

    base.html for template styling. Create-recipe.html

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+# Render homepage/index
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Render meal-search
+@app.route('/meal_search')
+def meal_search():
+    return render_template('meal-search.html')
+
+
+# Render create-recipe
+@app.route('/create_recipe', methods=["GET", "POST"])
+def create_recipe():
+    return render_template('create-recipe.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
