7049307b617efe2a9368d7d752426f1f6f928584
Alex Ruma <alexanderruma@gmail.com>
2020-11-07 16:02:50 -0800

    POST request handling for create-recipe

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
@@ -16,8 +16,24 @@ def meal_search():
 # Render create-recipe
 @app.route('/create_recipe', methods=["GET", "POST"])
 def create_recipe():
+    if request.method == "POST":
+        
+        # Contains name entered in form name input.
+        recipe_name = request.form["recipe_name"]
+        
+        # Populate list of checked ingrednients
+        ingredient_list =[]
+        for key in request.form:
+            if request.form[key]:
+                ingredient_list.append(request.form[key])
+
+        return redirect(url_for("new_recipe", new_recipe_name = recipe_name, ingredients = ingredient_list))
     return render_template('create-recipe.html')
 
+# Render page with user created recipe
+@app.route("/<new_recipe_name>/<ingredients>")
+def new_recipe(new_recipe_name, ingredients):
+    return render_template('new_recipe.html', recipe_name = new_recipe_name, recipe_ingredients = ingredients)
 
 if __name__ == "__main__":
     app.run(debug = True)
